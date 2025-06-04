190eea10853c07b76f9e3184bf7a49a9d511b94a
travezvinueza <travezvinueza@gmail.com>
2025-06-04 10:55:03 -0500

    feat: implement GraphQL support for user management and enhance user retrieval with pagination

--- src/main/java/com/develop/backend/domain/service/UserService.java
+++ src/main/java/com/develop/backend/domain/service/UserService.java
@@ -1,17 +1,17 @@
 package com.develop.backend.domain.service;
 
 import com.develop.backend.application.dto.UserDto;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface UserService {
     UserDto updateUser(UserDto userDto, MultipartFile newImage) throws IOException;
 
     void deleteUser(Long id);
 
-    List<UserDto> getAllUsers();
+    Page<UserDto> getAllUsers(String username, int page, int size);
 
     UserDto getUserById(Long id);
 
